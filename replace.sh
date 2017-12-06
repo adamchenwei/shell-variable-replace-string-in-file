ed7a9f2a00a9112c6fc0b3c3c9627de987057f46
Adam Chen Wei <awei@pebblepost.com>
2017-12-06 14:33:31 -0500

    chore(): tweak

--- replace.sh
+++ replace.sh
@@ -1,6 +1,6 @@
-MyIP=`ifconfig | grep "broadcast"| head -n1 | cut -d " " -f2`
-echo $MyIP
+myip=`ifconfig | grep "broadcast"| head -n1 | cut -d " " -f2`
+echo $myip
 
-sed -i "s/proxy_pass http:\/\/10.40.4.134:9010\/api\/login;/proxy_pass http:\/\/"$MyIP":9010\/api\/login;/g" targetfile.txt
+sed -i "s/proxy_pass http:\/\/10.40.4.134:9010\/api\/login;/proxy_pass http:\/\/"$myip":9010\/api\/login;/g" targetfile.txt
 
 #cat targetfile.txt | sed -e "s/MyIp/$MyIP" >> result.txt
