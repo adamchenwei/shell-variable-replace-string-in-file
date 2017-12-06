d86179a0fc66cd86afc5fa0ad8bac8950e43bd07
Adam Chen Wei <awei@pebblepost.com>
2017-12-06 14:46:18 -0500

    feat(replace): both in mac and ubuntu

---
+++ replaceOnMac.sh
@@ -0,0 +1,7 @@
+myip=`ifconfig | grep "broadcast"| head -n1 | cut -d " " -f2`
+echo 'ip below'
+echo $myip
+echo 'ip above'
+
+#mac
+sed -i -e "s/proxy_pass http:\/\/myip\/api\/login;/proxy_pass http:\/\/"$myip":9010\/api\/login;/g" targetfile.txt
--- replaceOnUbuntu.sh
+++ replaceOnUbuntu.sh
@@ -1,6 +1,7 @@
 myip=`ifconfig | grep "broadcast"| head -n1 | cut -d " " -f2`
+echo 'ip below'
 echo $myip
+echo 'ip above'
 
+#ubuntu
 sed -i "s/proxy_pass http:\/\/10.40.4.134:9010\/api\/login;/proxy_pass http:\/\/"$myip":9010\/api\/login;/g" targetfile.txt
-
-#cat targetfile.txt | sed -e "s/MyIp/$MyIP" >> result.txt
