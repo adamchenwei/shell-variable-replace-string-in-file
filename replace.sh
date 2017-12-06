3c4b7c23b66443dfcddb0348d6eb0ce26a5875fb
Adam Chen Wei <awei@pebblepost.com>
2017-12-06 14:31:14 -0500

    feat(ubuntu): supposely only run on ubuntu

--- replace.sh
+++ replace.sh
@@ -1,4 +1,6 @@
 MyIP=`ifconfig | grep "broadcast"| head -n1 | cut -d " " -f2`
 echo $MyIP
 
-cat targetfile.txt | sed -e "s/MyIp/$MyIP" >> result.txt
+sed -i "s/proxy_pass http:\/\/10.40.4.134:9010\/api\/login;/proxy_pass http:\/\/"$MyIP":9010\/api\/login;/g" targetfile.txt
+
+#cat targetfile.txt | sed -e "s/MyIp/$MyIP" >> result.txt
