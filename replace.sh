4089eec86530e2fba15b94a8c9fac43faf25be7b
Adam Chen Wei <awei@pebblepost.com>
2017-12-06 14:17:05 -0500

    chore(); still not working

--- replace.sh
+++ replace.sh
@@ -1,4 +1,4 @@
 MyIP=`ifconfig | grep "broadcast"| head -n1 | cut -d " " -f2`
 echo $MyIP
 
-cat targetfile.txt | sed -e "s/$MyIP/" >> result.txt
+cat targetfile.txt | sed -e "s/MyIp/$MyIP" >> result.txt
